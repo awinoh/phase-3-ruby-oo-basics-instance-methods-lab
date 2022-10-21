dc7526aeeaaf23fdc4f9d3c5e7ccda095cdb38b6
+ <awino.millicent@student.moringaschool.com>
2022-10-21 12:49:04 +0300

    completed assignmrnt

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog 
+    def bark
+        puts "Woof!"
+    end
+end
+
+class Dog 
+    def bark 
+        puts "Woof!"
+    end
+end
+
+fido = Dog.new
+fido.bark
+
+snoopy = Dog.new
+snoopy.bark
+
+class Dog
+    def sit 
+        puts "The Dog is sitting"
+    end
+end
+
+snoopy = Dog.new
+snoopy.sit
---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person 
+    def talk
+        puts "Hello World!"
+    end
+end 
+
+Nicole = Person.new
+
+class Person
+    def walk 
+        puts "The Person is walking"
+    end
+end
+Nicole = Person.new
+Nicole.walk
